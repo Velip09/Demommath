4018634436df6b403ccb171dc9a38861163e08bc
Velip09 <56682596+Velip09@users.noreply.github.com>
2024-01-11 15:32:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='demommath',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     long_description_content_type="text/markdown",
     long_description=open("README.md", "r", encoding="utf-8").read(),
